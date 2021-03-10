ff61f799a120530050adb258a80fbb4a5a0f37be
nwakkun <wataru011926@gmail.com>
2021-03-10 14:48:00 +0900

    登録機能追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,8 +1,26 @@
 class Admin::UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user = User.save
+      redirect_to admin_user_url(@user), notice: "ユーザー 「#{@user.name}」を登録しました。"
+    else
+      render :new
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :admin, :password, :password_digest)
+    end
   end
 
   def edit
+
   end
 
   def show
